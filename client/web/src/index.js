308ba1dbe56dab9dd728f5eae55ccf5fbe90ce06
ktr0731 <aizu.s1230022@gmail.com>
2018-06-22 16:54:45 +0900

    use babel 6

--- client/web/src/index.js
+++ client/web/src/index.js
@@ -1,15 +1,16 @@
-import { grpc } from 'grpc-web-client';
-import { Example, SimpleRequest } from './api_pb.js';
-
-const req = new SimpleRequest();
-req.setName("ktr");
-grpc.unary(Example.Unary, {
-    request: req,
-    host: "localhost:50051",
-    onEnd: res => {
-        const { status, statusMessage, headers, message, trailers } = res;
-        if (status === grpc.Code.OK && message) {
-            console.log("all ok. got book: ", message.toObject());
-        }
-    }
-});
+console.log("hello")
+// import { grpc } from 'grpc-web-client';
+// import { Example, SimpleRequest } from './api_pb.js';
+//
+// const req = new SimpleRequest();
+// req.setName("ktr");
+// grpc.unary(Example.Unary, {
+//     request: req,
+//     host: "localhost:50051",
+//     onEnd: res => {
+//         const { status, statusMessage, headers, message, trailers } = res;
+//         if (status === grpc.Code.OK && message) {
+//             console.log("all ok. got book: ", message.toObject());
+//         }
+//     }
+// });
